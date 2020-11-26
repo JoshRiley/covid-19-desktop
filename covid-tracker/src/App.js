490c12a261add346258d68756d89511634f62924
Josh Riley <jriley@tfdidesign.com>
2020-11-26 02:18:39 +0000

    Configured application for use in react

---
+++ covid-tracker/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://cavernoma.org.uk/wp-content/uploads/2020/09/covid-19.png" className="App-logo" alt="logo" />
+        <p>
+          COVID-19 Desktop Stats Tracker
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
